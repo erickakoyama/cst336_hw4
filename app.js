60a8bda6b64ba1cf7c8b9004bd16a60c574ca04f
erickakoyama <ericka.koyama@gmail.com>
2020-11-23 00:31:11 +0000

    wip

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
+var faker = require('faker');
 const express = require('express');
 const app = express();
 app.engine('html', require('ejs').renderFile);
 app.use(express.static('public'));
 
+const fakeCompany = faker.company.companyName();
+
 // routes
 app.get('/', (req, res) => {
-  res.render('index.ejs', { activeRoute: '/' });
+  res.render('index.ejs', {
+    activeRoute: '/',
+    fakeCompany
+  });
 });
 
 app.get('/deep-space-network', (req, res) => {
-  res.render('deep-space-network.ejs', { activeRoute: 'deep-space-network' });
+  res.render('deep-space-network.ejs', {
+    activeRoute: 'deep-space-network',
+    fakeCompany
+  });
 });
 
 app.get('/interplanetary-internet', (req, res) => {
-  res.render('interplanetary-internet.ejs', { activeRoute: 'interplanetary-internet' });
+  res.render('interplanetary-internet.ejs', {
+    activeRoute: 'interplanetary-internet',
+    fakeCompany
+  });
 });
 
 app.get('/satellite-internet', (req, res) => {
-  res.render('satellite-internet.ejs', { activeRoute: 'satellite-internet' });
+  res.render('satellite-internet.ejs', {
+    activeRoute: 'satellite-internet',
+    fakeCompany
+  });
 });
 
 
