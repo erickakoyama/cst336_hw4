d5059d825e8c866cc8a880a72486f494d2b86187
erickakoyama <ericka.koyama@gmail.com>
2020-11-23 00:18:53 +0000

    wip

--- app.js
+++ app.js
@@ -5,7 +5,19 @@ app.use(express.static('public'));
 
 // routes
 app.get('/', (req, res) => {
-  res.render('index.html');
+  res.render('index.ejs', { activeRoute: '/' });
+});
+
+app.get('/deep-space-network', (req, res) => {
+  res.render('deep-space-network.ejs', { activeRoute: 'deep-space-network' });
+});
+
+app.get('/interplanetary-internet', (req, res) => {
+  res.render('interplanetary-internet.ejs', { activeRoute: 'interplanetary-internet' });
+});
+
+app.get('/satellite-internet', (req, res) => {
+  res.render('satellite-internet.ejs', { activeRoute: 'satellite-internet' });
 });
 
 
